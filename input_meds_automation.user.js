0344f971e77d5404946c0d28ba80356f38b51659
Bryson Marazzi <bryson.marazzi@gmail.com>
2023-07-13 10:18:30 -0700

    First commit of input meds file and test

---
+++ input_meds_automation.user.js
@@ -0,0 +1,306 @@
+// ==UserScript==
+// @name         Medications Input Automation
+// @namespace    http://tampermonkey.net/
+// @version      0.0
+// @description  Automate insertion of medical data into Arya
+// @author       Bryson Marazzi
+// @match        https://app.aryaehr.com/aryaehr/clinics/*
+// @icon         https://static.wixstatic.com/media/655afa_e1a9bb3939634fe2a263d24ef95da02b~mv2.png/v1/fill/w_146,h_150,al_c,q_85,enc_auto/655afa_e1a9bb3939634fe2a263d24ef95da02b~mv2.png
+// @grant        none
+// ==/UserScript==
+
+const MEDICATION_ID = "medications";
+const ARYA_URL_ROOT = 'https://app.aryaehr.com/api/v1/';
+const CLINIC_ID_INDEX = 5;
+const WARNING_COLOR = '#E63B16';
+const SUCCESS_COLOR = '#228B22';
+
+'use strict';
+
+document.addEventListener('keydown', function(event) {
+    window.clinic_id = window.location.href.split("/")[CLINIC_ID_INDEX];
+    // Check if Command+V (Mac) or Ctrl+V (Windows) is pressed
+    if ((event.metaKey || event.ctrlKey) && event.code === 'KeyV') {
+        // Access clipboard data
+        navigator.clipboard.readText().then(function(text) {
+            // Use the clipboard data here
+            let medicalData = parseMedicalData(text);
+            if (medicalData){
+                getPatientData(medicalData.PHN)
+                .then(patientData => {
+                    return handlePatientMedicalRecords(patientData, medicalData);
+                })
+                    .catch(error => console.error(error));
+            } else {
+                warningAlert("Pasted Pharmanet string is incorrectly formatted!")
+            }
+        }).catch(function(error) {
+            console.error('Failed to read clipboard data:', error);
+        });
+    }
+});
+
+function getPatientData(phn){
+    return fetch(ARYA_URL_ROOT + "clinics/" + window.clinic_id + '/patients.json?limit=1&offset=0&term=' + phn.replace(/\s/g, ""), {
+        method: 'GET',
+    })
+        .then(response => response.json())
+        .then(jsonList => {
+        if (jsonList.length !== 1) {
+            window.alert("There is no patient found in Arya with PHN="+phn);
+            throw new Error("There is no patient found in Arya with PHN="+phn);
+        }
+        return jsonList[0];
+    })
+}
+
+function handlePatientMedicalRecords(patientData, medicalData){
+    console.log("Handle Medical Records");
+    console.log(patientData);
+    console.log(medicalData);
+    let medicationPromises = medicalData.medications.map(medication => {
+        return lookForSuggestionMatch(medication)
+        .then(match => { medication.match = match; return medication; })
+    })
+    Promise.all(medicationPromises).then(medications => {
+        console.log("Promised all the medications: ");
+        console.log(medications);
+        return medications;
+    })
+    .then(medications => medications.filter(med => med.match && med.current))
+    .then(matchedMedications => {
+        console.log("Filtered Medications for HAS MATCH && IS CURRENT: ");
+        console.log(matchedMedications);
+        return matchedMedications;
+    })
+    .then(medicationsToInsert => {
+        // Prompt the user if insertion is good
+        const sep = "\n===============================";
+        const message = "Inserting into Patient: " + patientData.label + sep +
+                "\nPress [OK] or [ENTER KEY] to insert " + medicationsToInsert.length + " medical records" +
+                "\nClick [CANCEL] to cancel" + sep +
+                "\nNOTE: This feature is still a WORK IN PROGRESS and won't always include every medical record from the paste!" ;
+        const userInput = window.prompt(message);
+
+        if (userInput === null) {
+            // User clicked Cancel or closed the prompt
+            warningAlert("Cancelled inserting medications records!")
+        } else {
+            // User pressed Enter key
+            console.log("Constructing then inserting into arya");
+            let aryaMeds = constructMedications(patientData, medicationsToInsert)
+            insertMedications(aryaMeds);
+        }
+    });
+}
+
+//Given medications that have already been filtered create the arya medication with patient data
+//TODO once not filtering out the non matches, implement contructing without the match.
+function constructMedications(patientData, medications){
+    return medications.map(medication => {
+        let match = medication.match;
+        return {
+            "patient_id": patientData.uuid,
+            "dose": match.strength_with_unit,
+            "route": match.route.route_of_administration_name,
+            "comment": medication.Instruction,
+            "name": match.ingredient_name,
+            "frequency": extractFrequencyFromInstruction(medication.Instruction)
+        }
+    });
+}
+
+//Given a list of arya medications, insert all into database
+function insertMedications(aryaMedications){
+    console.log("Inserting medications into Arya");
+    let insertPromises = aryaMedications.map(medication => insertMedication(medication));
+    return Promise.all(insertPromises).then(insertedRecords => {
+        console.log(insertedRecords);
+        successAlert("Successfully inserted " + insertedRecords.length + " medical records. Refresh to see.");
+    }).catch(error => console.error(error));
+}
+
+//Extract the MG dosage from the name and remove the space. Null if doesn't exist.
+function extractDoseFromName(name){
+    const match = name.match(/(\d+)\s+MG/);
+    if (match && match.length > 0) {
+        return match[0].replace(/\s/g, "");
+    }
+    return null;
+}
+
+function extractFrequencyFromInstruction(instruction){
+    if(instruction.includes("ONCE")){ return "Daily"; }
+    if(instruction.includes("TWICE")){ return "BID"; }
+    if(instruction.includes("THREE")){ return "TID"; }
+    if(instruction.includes("FOUR")){ return "QID"; }
+    return "PRN";
+}
+
+function extractSearchWord(medication){
+    return medication.name.split(" ")[0];
+}
+
+// Given a medication, check for suggestion matches
+// Based on drug name and dosage
+function lookForSuggestionMatch(medication){
+
+    // Get the first word from the Name
+    let searchTerm = extractSearchWord(medication);
+
+    //Extract the dosage from the name if exists. Format as 50MG or 20MG or null.
+    let dosage = extractDoseFromName(medication.Name);
+
+    if (!dosage){ console.log("DOSAGE NOT FOUND IN DRUG NAME: " + medication.Name) }
+
+    return fetch(ARYA_URL_ROOT + 'drugs?limit=50&offset=0&search=' + searchTerm, { method: 'GET', })
+        .then(response => response.json())
+        .then(matches => matches.find(match => match.strength_with_unit === dosage && dosage ))
+        .then(match => { if(match === undefined){ return null } else { return match } })
+        .catch(error => console.error(error));
+}
+
+//Given a valid arya style medication, insert into the backend
+//TODO Make sure the data is not already in the database.
+function insertMedication(medication){
+    console.log("Inserting into Arya: " + JSON.stringify(medication));
+
+    // Define the request payload
+    const payload = { medication: medication };
+
+    // Make the POST request
+    return fetch(ARYA_URL_ROOT + 'clinics/' + window.clinic_id + '/medications', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(payload)
+    })
+        .then(response => response.json());
+}
+
+function parseMedicalData(dataString) {
+    function parseMedicationLines(isCurrent, lines){
+        const medication = {
+            current: isCurrent,
+            DIN: undefined,
+            Name: undefined,
+            Instruction: undefined
+        };
+        medication.DIN = lines[0].trim();
+        medication.Name = lines[1].trim();
+        medication.Instruction = lines[5].trim();
+        return medication;
+    }
+
+    //Given the input string split into lines, return true if represents data copied form Pharmanet data, false otherwise
+    function isValidMedicalData(lines){
+        return (lines && lines.length > 0 && lines[0].trim().startsWith("Request issued") && lines.length > 13);
+    }
+
+    const CURRENT_MR_LENGTH = 8;
+    const NON_CURRENT_MR_LENGTH = 6;
+    function isDINOrContinue(string) {
+        let trimmed = string.trim();
+        return /^\d+$/.test(trimmed) || trimmed === 'Continue';
+    }
+
+    const medicalData = {
+        PHN: '',
+        name: '',
+        birthDate: '',
+        gender: '',
+        medications: []
+    };
+
+    const lines = dataString.split('\n');
+
+    //Validate the input and return early if not valid
+    if(!isValidMedicalData(lines)){ return null; }
+
+    let isParsingMedications = false;
+
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i].trim();
+
+        if (line === 'Continue') {
+            if (isParsingMedications) {
+                break; // Stop parsing once second "Continue" line is found
+            } else {
+                isParsingMedications = true; // Start parsing medical records
+                continue;
+            }
+        }
+
+        if (!isParsingMedications) {
+            const patientInfoRegex = /(\d{4} \d{3} \d{3}) - (.+) - (\d{4} [a-zA-Z]{3} \d{2}) - (\w+)/;
+            const patientInfoMatch = patientInfoRegex.exec(line);
+
+            if (patientInfoMatch) {
+                medicalData.PHN = patientInfoMatch[1];
+                medicalData.name = patientInfoMatch[2];
+                medicalData.birthDate = patientInfoMatch[3];
+                medicalData.gender = patientInfoMatch[4];
+            }
+        } else {
+
+            let isCurrent;
+            let medicationDataLines;
+
+            if (i + CURRENT_MR_LENGTH < lines.length && isDINOrContinue(lines[i + CURRENT_MR_LENGTH])){
+                isCurrent = true;
+                medicationDataLines = lines.slice(i, i + CURRENT_MR_LENGTH);
+            } else if (i + NON_CURRENT_MR_LENGTH < lines.length && isDINOrContinue(lines[i + NON_CURRENT_MR_LENGTH])){
+                isCurrent = false;
+                medicationDataLines = lines.slice(i, i + NON_CURRENT_MR_LENGTH);
+            } else {
+                break;
+            }
+
+            if (isCurrent) { medicationDataLines.splice(1,2); }
+
+            let medication = parseMedicationLines(isCurrent, medicationDataLines);
+
+            i += (isCurrent ? CURRENT_MR_LENGTH : NON_CURRENT_MR_LENGTH) - 1;
+
+            medicalData.medications.push(medication);
+        }
+    }
+
+    return medicalData;
+}
+
+function getMedicationList(patient_id){
+    console.log("getMedicationList:");
+    return getPatientData(patient_id)
+    .then(data => data.medications)
+}
+
+function warningAlert(message){
+    showNonBlockingAlert(message, WARNING_COLOR);
+}
+function successAlert(message){
+    showNonBlockingAlert(message, SUCCESS_COLOR);
+}
+function showNonBlockingAlert(message, color) {
+    const alertDiv = document.createElement('div');
+    alertDiv.innerHTML = message;
+    alertDiv.style.position = 'fixed';
+    alertDiv.style.top = '96%';
+    alertDiv.style.left = '50%';
+    alertDiv.style.transform = 'translate(-50%, -50%)';
+    alertDiv.style.backgroundColor = color;
+    alertDiv.style.color = '#fff';
+    alertDiv.style.padding = '10px 20px';
+    alertDiv.style.borderRadius = '4px';
+    alertDiv.style.zIndex = '9999';
+    alertDiv.style.opacity = '1.9';
+    alertDiv.style.transition = 'opacity 0.5s';
+    
+    document.body.appendChild(alertDiv);
+    
+    setTimeout(function() {
+        alertDiv.style.opacity = '0';
+        setTimeout(function() {
+            document.body.removeChild(alertDiv);
+        }, 500);
+    }, 2000); // Show the alert for 2 seconds
+}
---
+++ medication_parsing_test.js
@@ -0,0 +1,367 @@
+const PATIENT_WITH_MCG_AND_DECIMAL = `Request issued 230713 by Prac: 91/13865 (Marazzi)    Logon ID: TEST_1234
+0 Operation successful
+ 
+
+Med reconciliation
+1234 123 123 - JOHN E DOE - 1992 Jan 01 - Male
+Please check the medications you want included on the report,
+then click the Continue Button.
+Drugs are listed in alphabetic order
+
+NOTE: By default only current medications are automatically selected
+
+ Check here to include all prescriptions on the report
+
+      Continue
+01938088
+
+
+	ACETAMINOPHEN    325 MG TABLET
+Jamp Acetaminophen 325   JAMP PHARMA CO
+Dispensed: 2023 Jul 12 Status:Filled
+Qty: 6.0 Duration: 1
+*DAILY DISPENSE* TAKE 2 TABLETS ORALLY THREE TIMES DAILY
+02487381
+
+
+	APIXABAN    2.5 MG TABLET
+Apo-Apixaban   APOTEX INC
+Dispensed: 2023 Jul 12 Status:Filled
+Qty: 2.0 Duration: 1
+*DAILY DISPENSE* TAKE 1 TABLET ORALLY TWICE DAILY
+02487403
+		APIXABAN    5 MG TABLET
+Apo-Apixaban   APOTEX INC
+Dispensed: 2023 Apr 25 Status:Filled
+Qty: 2.0 Duration: 1
+*DAILY DISPENSE* TAKE 1 TABLET ORALLY TWICE DAILY
+02489236
+		APIXABAN    5 MG TABLET
+Sandoz Apixaban Sdz   SANDOZ CANADA
+Dispensed: 2023 Feb 24 Status:Filled
+Qty: 60.0 Duration: 30
+TAKE 1 TABLET TWICE DAILY (AT 8AM AND 8PM)
+02245916
+		CARVEDILOL    12.5 MG TABLET
+Pms-Carvedilol   PHARMASCIENCE
+Dispensed: 2023 Jan 27 Status:Filled
+Qty: 90.0 Duration: 90
+TAKE 1/2 TABLET TWICE A DAY
+02245916
+		CARVEDILOL    12.5 MG TABLET
+Pms-Carvedilol   PHARMASCIENCE
+Dispensed: 2023 Feb 24 Status:Filled
+Qty: 30.0 Duration: 30
+TAKE 1/2 TABLET TWICE A DAY WITH FOOD
+66123252
+		COMPOUNDED PREP (PHARMACARE NO   
+Compounded Prep (Pharmacare Non-Benefit)   UNKNOWN
+Dispensed: 2023 May 15 Status:Filled
+Qty: 400.0 Duration: 14
+(ACETAMINOPHEN 100MG/ML ) (SHAKE WELL ) THEN GIVE 6.5 MILLILITERS (=650MG) FOU
+02531410
+
+
+	DAPAGLIFLOZIN    10 MG TABLET
+AURO PHARMA IN
+Dispensed: 2023 Jul 12 Status:Filled
+Qty: 1.0 Duration: 1
+* DAILY DISPENSE* TAKE ONE TABLET ORALLY ONCE DAILY ( NEW MED)
+02335700
+		DIGOXIN    0.0625 MG TABLET
+Pms-Digoxin   PHARMASCIENCE
+Dispensed: 2023 Feb 24 Status:Filled
+Qty: 30.0 Duration: 30
+TAKE 1 TABLET ONCE A DAY
+02335719
+
+
+	DIGOXIN    0.125 MG TABLET
+Pms-Digoxin   PHARMASCIENCE
+Dispensed: 2023 Jul 11 Status:Filled
+Qty: 1.0 Duration: 1
+*DAILY DISPENSE* TAKE 1 TABLET ORALLY ONCE DAILY
+02466759
+		FUROSEMIDE    20 MG TABLET
+Mint-Furosemide   MINT PHARMACEU
+Dispensed: 2023 Apr 25 Status:Filled
+Qty: 3.0 Duration: 1
+*DAILY DISPENSE* TAKE 3 TABLETS ORALLY ONCE DAILY
+02466759
+
+
+	FUROSEMIDE    20 MG TABLET
+Mint-Furosemide   MINT PHARMACEU
+Dispensed: 2023 Jul 12 Status:Filled
+Qty: 3.0 Duration: 1
+*DAILY DISPENSE* TAKE 3 TABLETS ORALLY ONCE DAILY (HOLD IF SBP LESS THAN 90 AND
+02466759
+		FUROSEMIDE    20 MG TABLET
+Mint-Furosemide   MINT PHARMACEU
+Dispensed: 2023 Feb 24 Status:Filled
+Qty: 60.0 Duration: 30
+TAKE 1 TABLET TWICE DAILY
+02466767
+		FUROSEMIDE    40 MG TABLET
+Mint-Furosemide   MINT PHARMACEU
+Dispensed: 2023 Jan 26 Status:Filled
+Qty: 90.0 Duration: 90
+TAKE 1 TABLET ONCE A DAY ( FOR FLUID RETENTION )
+00885444
+		HYDROMORPHONE HCL    1 MG TABLET
+Pms-Hydromorphone Tab 1mg   PHARMASCIENCE
+Dispensed: 2023 May 20 Status:Filled
+Qty: 1.0 Duration: 1
+(MAY 16 - JUNE 07 )DAILY DISPENSE* TAKE 1/2 TABLET ORALLY FOUR TIMES DAILY AS NE
+00885444
+		HYDROMORPHONE HCL    1 MG TABLET
+Pms-Hydromorphone Tab 1mg   PHARMASCIENCE
+Dispensed: 2023 May 18 Status:Filled
+Qty: 0.5 Duration: 1
+(MAY 16 - JUNE 24)DAILY DISPENSE* TAKE 1/2 TABLET ORALLY FOUR TIMES DAILY AS NEE
+00885444
+		HYDROMORPHONE HCL    1 MG TABLET
+Pms-Hydromorphone Tab 1mg   PHARMASCIENCE
+Dispensed: 2023 May 04 Status:Filled
+Qty: 2.0 Duration: 1
+*DAILY DISPENSE* TAKE 1/2 TABLET ORALLY FOUR TIMES DAILY AS NEEDED (4 DOSES IN S
+00885444
+		HYDROMORPHONE HCL    1 MG TABLET
+Pms-Hydromorphone Tab 1mg   PHARMASCIENCE
+Dispensed: 2023 Apr 26 Status:Filled
+Qty: 1.5 Duration: 1
+*DAILY DISPENSE* TAKE 1/2 TABLET ORALLY FOUR TIMES DAILY AS NEEDED (PT ONLY WANT
+02172062
+
+
+	LEVOTHYROXINE SODIUM    25 MCG TABLET
+Synthroid   BGP PHARMA ULC
+Dispensed: 2023 Jul 12 Status:Filled
+Qty: 1.0 Duration: 1
+*DAILY DISPENSE* TAKE 1 TABLET ORALLY ONCE DAILY {LEVOTHYROXINE}
+99000501
+		MEDICATION REVIEW STANDARD    
+Medication Review Standard   UNKNOWN
+Dispensed: 2023 May 12 Status:Filled
+Qty: 1.0 Duration: 1
+604-574-3331 MEDICATION REVIEW PHARMACITY DRUGSTORE #3 **(MR-S)**
+02248855
+
+
+	METOPROLOL TARTRATE    25 MG TABLET
+Pms-Metoprolol-L   PHARMASCIENCE
+Dispensed: 2023 Jul 11 Status:Filled
+Qty: 1.0 Duration: 1
+*DAILY DISPENSE* TAKE 1/2 TABLET ORALLY TWICE DAILY
+02384892
+		NABILONE    1 MG CAPSULE
+Teva-Nabilone   TEVA CANADA LI
+Dispensed: 2023 Jun 17 Status:Filled
+Qty: 30.0 Duration: 5
+TAKE 1 OR 2 CAPSULES 3 TIMES A DAY
+02384892
+		NABILONE    1 MG CAPSULE
+Teva-Nabilone   TEVA CANADA LI
+Dispensed: 2023 May 19 Status:Filled
+Qty: 30.0 Duration: 5
+TAKE 1 OR 2 CAPSULES 3 TIMES A DAY AS NEEDED
+02408570
+		PANTOPRAZOLE MAGNESIUM    40 MG TABLET DR
+Mylan-Pantoprazole T   MYLAN PHARMACE
+Dispensed: 2023 May 19 Status:Filled
+Qty: 30.0 Duration: 30
+TAKE 1 TABLET ONCE A DAY
+02316080
+
+
+	QUETIAPINE FUMARATE    25 MG TABLET
+Act Quetiapine   TEVA CANADA LI
+Dispensed: 2023 Jul 12 Status:Filled
+Qty: 0.5 Duration: 1
+*DAILY DISPENSE* TAKE 1/2 TABLET ORALLY AT BEDTIME
+02446936
+
+
+	SACUBITRIL/VALSARTAN    49 MG-51MG TABLET
+Entresto   NOVARTIS PHARM
+Dispensed: 2023 Jul 12 Status:Filled
+Qty: 2.0 Duration: 1
+*DAILY DISPENSE* TAKE 1 TABLET ORALLY TWICE DAILY (SACUBATRIL/VALSARTAN)
+02446944
+		SACUBITRIL/VALSARTAN    97MG-103MG TABLET
+Entresto   NOVARTIS PHARM
+Dispensed: 2023 Jan 30 Status:Filled
+Qty: 100.0 Duration: 50
+TAKE 1 TABLET TWICE DAILY
+02446944
+		SACUBITRIL/VALSARTAN    97MG-103MG TABLET
+Entresto   NOVARTIS PHARM
+Dispensed: 2023 Feb 24 Status:Filled
+Qty: 60.0 Duration: 30
+TAKE 1 TABLET TWICE DAILY (START IF SYSTOLIC BLOOD PRESSURE IS GREATER THAN 100)
+      Continue
+
+ `
+
+ const PATIENT_WITH_DECIMAL_AND_TEMP_DRUG = `Request issued 230713 by Prac: 91/13865 (TESTER)    Logon ID: TEST1234
+0 Operation successful
+ 
+
+Med reconciliation
+4567 111 222 - MARY U DOE - 1955 Jan 02 - Female
+Please check the medications you want included on the report,
+then click the Continue Button.
+Drugs are listed in alphabetic order
+
+NOTE: By default only current medications are automatically selected
+
+ Check here to include all prescriptions on the report
+
+      Continue
+02489236
+
+
+	APIXABAN    5 MG TABLET
+Sandoz Apixaban Sdz   SANDOZ CANADA
+Dispensed: 2023 Jul 08 Status:Filled
+Qty: 56.0 Duration: 28
+TAKE 1 TABLET TWICE A DAY
+02272873
+
+
+	CARBIDOPA/LEVODOPA    25MG-100MG TABLET ER
+Aa-Levocarb Cr   AA PHARMA INC.
+Dispensed: 2023 Jul 08 Status:Filled
+Qty: 140.0 Duration: 28
+TAKE 1 TABLET ORALLY FOUR TIMES A DAY
+02521261
+		CEPHALEXIN    500 MG TABLET
+Cephalexin   SANIS HEALTH
+Dispensed: 2023 Apr 28 Status:Filled
+Qty: 28.0 Duration: 7
+TAKE 1 TABLET EVERY 6 HOURS FOR 7 DAYS
+02335719
+
+
+	DIGOXIN    0.125 MG TABLET
+Pms-Digoxin   PHARMASCIENCE
+Dispensed: 2023 Jul 08 Status:Filled
+Qty: 28.0 Duration: 28
+TAKE 1 TABLET ONCE DAILY
+02516136
+
+
+	DILTIAZEM HCL    240 MG CAP SA 24H
+Diltiazem T   SANIS HEALTH
+Dispensed: 2023 Jul 08 Status:Filled
+Qty: 28.0 Duration: 28
+TAKE 1 CAPSULE ORALLY ONCE DAILY
+02351420
+
+
+	FUROSEMIDE    20 MG TABLET
+Furosemide   SANIS HEALTH
+Dispensed: 2023 Jul 09 Status:Filled
+Qty: 28.0 Duration: 28
+TAKE 1 TABLET ONCE DAILY
+      Continue
+
+ `
+
+function parseMedicalData(dataString) {
+    function parseMedicationLines(isCurrent, lines){
+        const medication = {
+            current: isCurrent,
+            DIN: undefined,
+            Name: undefined,
+            Instruction: undefined
+        };
+        medication.DIN = lines[0].trim();
+        medication.Name = lines[1].trim();
+        medication.Instruction = lines[5].trim();
+        return medication;
+    }
+
+    //Given the input string split into lines, return true if represents data copied form Pharmanet data, false otherwise
+    function isValidMedicalData(lines){
+        return (lines && lines.length > 0 && lines[0].trim().startsWith("Request issued") && lines.length > 13);
+    }
+
+    const CURRENT_MR_LENGTH = 8;
+    const NON_CURRENT_MR_LENGTH = 6;
+    function isDINOrContinue(string) {
+        let trimmed = string.trim();
+        return /^\d+$/.test(trimmed) || trimmed === 'Continue';
+    }
+
+    const medicalData = {
+        PHN: '',
+        name: '',
+        birthDate: '',
+        gender: '',
+        medications: []
+    };
+
+    const lines = dataString.split('\n');
+
+    //Validate the input and return early if not valid
+    if(!isValidMedicalData(lines)){ return null; }
+
+    let isParsingMedications = false;
+
+    for (let i = 0; i < lines.length; i++) {
+        const line = lines[i].trim();
+
+        if (line === 'Continue') {
+            if (isParsingMedications) {
+                break; // Stop parsing once second "Continue" line is found
+            } else {
+                isParsingMedications = true; // Start parsing medical records
+                continue;
+            }
+        }
+
+        if (!isParsingMedications) {
+            const patientInfoRegex = /(\d{4} \d{3} \d{3}) - (.+) - (\d{4} [a-zA-Z]{3} \d{2}) - (\w+)/;
+            const patientInfoMatch = patientInfoRegex.exec(line);
+
+            if (patientInfoMatch) {
+                medicalData.PHN = patientInfoMatch[1];
+                medicalData.name = patientInfoMatch[2];
+                medicalData.birthDate = patientInfoMatch[3];
+                medicalData.gender = patientInfoMatch[4];
+            }
+        } else {
+
+            let isCurrent;
+            let medicationDataLines;
+
+            if (i + CURRENT_MR_LENGTH < lines.length && isDINOrContinue(lines[i + CURRENT_MR_LENGTH])){
+                isCurrent = true;
+                medicationDataLines = lines.slice(i, i + CURRENT_MR_LENGTH);
+            } else if (i + NON_CURRENT_MR_LENGTH < lines.length && isDINOrContinue(lines[i + NON_CURRENT_MR_LENGTH])){
+                isCurrent = false;
+                medicationDataLines = lines.slice(i, i + NON_CURRENT_MR_LENGTH);
+            } else {
+                break;
+            }
+
+            if (isCurrent) { medicationDataLines.splice(1,2); }
+
+            let medication = parseMedicationLines(isCurrent, medicationDataLines);
+
+            i += (isCurrent ? CURRENT_MR_LENGTH : NON_CURRENT_MR_LENGTH) - 1;
+
+            medicalData.medications.push(medication);
+        }
+    }
+
+    return medicalData;
+}
+
+let result = parseMedicalData(PATIENT_WITH_DECIMAL_AND_TEMP_DRUG);
+console.log(result);
+
+
+let result2 = parseMedicalData(PATIENT_WITH_MCG_AND_DECIMAL);
+console.log(result2);
