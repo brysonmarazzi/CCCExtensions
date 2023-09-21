3236564eed32fa9d886a6dc6fe46893eae4ccafd
Bryson Marazzi <bryson.marazzi@gmail.com>
2023-09-20 22:09:46 -0700

    First prod ready version of results

--- results_number.user.js
+++ results_number.user.js
@@ -1,15 +1,14 @@
 // ==UserScript==
 // @name         Arya Results Number
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @version      1.0
+// @description  Logs the number of Results for a Doctor
+// @author       Bryson Marazzi
 // @match        https://app.aryaehr.com/aryaehr/clinics/*/results
 // @icon         https://static.wixstatic.com/media/655afa_e1a9bb3939634fe2a263d24ef95da02b~mv2.png/v1/fill/w_146,h_150,al_c,q_85,enc_auto/655afa_e1a9bb3939634fe2a263d24ef95da02b~mv2.png
-// @grant        none
 // ==/UserScript==
 
-const MEDICATION_ID = "medications";
+const IS_RESULTS_PAGE_REGEX = /^https:\/\/app\.aryaehr\.com\/aryaehr\/clinics\/[a-zA-Z0-9-]+\/results$/;
 const ARYA_URL_ROOT = 'https://app.aryaehr.com/api/v1//clinics/';
 const PHARMA_URL_ROOT = 'https://swan.medinet.ca/cgi-bin/launch.cgi';
 const PATIENT_ID_INDEX = 7;
@@ -18,59 +17,91 @@ const MAX_PAGE_SIZE = 50;
 
 (function() {
     'use strict';
-    //id="mat-select-value-1"
-    //selectuser
-    // Usage example:
+    const numberNode = document.createTextNode("-");
     window.clinic_id = window.location.href.split("/")[CLINIC_ID_INDEX];
 
-    waitForElement("selectuser", function(select_user){
+    window.onload = observeUrlChange(IS_RESULTS_PAGE_REGEX, onPageLoad);
 
-        const htmlString = '<span class="green_text"><button mat-flat-button="" class="mat-focus-indicator mat-flat-button mat-button-base"><span id="bryson_button_span" class="mat-button-wrapper">Show Total</span><span matripple="" class="mat-ripple mat-button-ripple"></span><span class="mat-button-focus-overlay"></span></button></span>'
-        const fragment = createFragmentFromHTML(htmlString);
+    function onPageLoad(){
+        waitForElement("selectuser", function(select_user){
+            insertTextNode();
+            observeCurrentUserSelection();
+            // observeCurrentList();
+            countCurrentUsersResults()
+            .then(updateResultsNumber);
+        });
+    }
 
-        // Create a button element
-        let button = fragment.querySelector("button");
-        console.log(fragment.querySelector(".mat-button-wrapper"));
+    function updateResultsNumber(number) {
+        numberNode.nodeValue = number
+    }
 
-        button.addEventListener('click', async function(event) {
-            const target = event.target;
-            console.log(target);
-            countCurrentUsersResults().then(count => {
-                let span = document.getElementById("bryson_button_span");
-                console.log(count);
-                console.log(span);
-                span.innerText = `${count}`;
+    function decrementResultsNumber() {
+        let num = Number(numberNode.nodeValue);
+        if(!isNaN(num)) {
+            numberNode.nodeValue = (num - 1).toString()
+        }
+    }
+
+    /**
+     * Work in progress -- When the user filters the results using the selection of "All Categories" to something else, then the 
+     * selected and active element is removed and the number decremenets. 
+     * Need to find a way to stop this. 
+     */
+    function observeCurrentList(){
+        // Create the Mutation Observer
+        const observer = new MutationObserver((mutationsList) => {
+            mutationsList.forEach(mutation => {
+                if (mutation.type === 'childList' && mutation.removedNodes.length > 0) {
+                    let removedNode = mutation.removedNodes[0];
+                    if(removedNode.nodeName != "#comment") {
+                        if(removedNode.classList.contains("active")) {
+                            console.log("Active Node Removed -- decrementing")
+                            decrementResultsNumber();
+                        }
+                    }
+                }
             });
-            //target.querySelector(".mat-button-wrapper")
         });
 
+        // Observe the target element (the span)
+        const target = document.querySelector('.efax_outbox_patient_list');
+        observer.observe(target, { 
+            characterDataOldValue: true,
+            subtree: true, 
+            childList: true,
+        });
 
-        // Find the parent container of the "Results" section
-        const parentContainer = document.querySelector('.top_heading');
+        return observer;
+    }
 
-        // Apply CSS styles to the parent container
-        parentContainer.style.display = 'flex';
-        parentContainer.style.alignItems = 'center';
-        parentContainer.style.justifyContent = 'space-between';
+    function observeCurrentUserSelection(){
+        // Create the Mutation Observer
+        const observer = new MutationObserver((mutationsList) => {
+            mutationsList.forEach(mutation => {
+                if (mutation.type === 'characterData' && mutation.target.textContent.length > 6) {
+                    updateResultsNumber("-");
+                    countCurrentUsersResults()
+                    .then(updateResultsNumber);
+                }
+            });
+        });
 
-        // Find the <h3> element inside the parent container
-        const heading = parentContainer.querySelector('h3');
+        // Observe the target element (the span)
+        const target = document.querySelector('.mat-select-value-text');
+        observer.observe(target, { 
+            characterDataOldValue: true,
+            subtree: true, 
+            childList: true,
+        });
+        return observer;
+    }
 
-        // Apply CSS styles to the <h3> element
-        heading.style.flex = '1';
-        heading.style.margin = '0';
-        heading.style.paddingRight = '10px';
-
-        // Apply CSS styles to the <button> element
-        button.style.marginLeft = '20px';
-
-        heading.appendChild(fragment);
-    });
-
-    function createFragmentFromHTML(htmlString) {
-        const range = document.createRange();
-        const fragment = range.createContextualFragment(htmlString);
-        return fragment;
+    function insertTextNode() {
+        const parentContainer = document.querySelector('.mat-select-value-text');
+        parentContainer.appendChild(document.createTextNode(" ["));
+        parentContainer.appendChild(numberNode);
+        parentContainer.appendChild(document.createTextNode("]"));
     }
 
     function countCurrentUsersResults(){
@@ -83,7 +114,11 @@ const MAX_PAGE_SIZE = 50;
         .then(user => {
             console.log("Current User:");
             console.log(user);
-            return countUusersResults(user.uuid);
+            if (user !== undefined) {
+                return countUusersResults(user.uuid);
+            }
+            console.error("Couldn't find user");
+            return "-"
         });
     }
 
@@ -147,7 +182,6 @@ const MAX_PAGE_SIZE = 50;
     }
 
     function waitForElement(element_id, callback) {
-        console.log("setting up waiting");
         const maxAttempts = 10;
         const initialDelay = 500; // milliseconds
         let attempt = 0;
@@ -164,8 +198,25 @@ const MAX_PAGE_SIZE = 50;
                 }
             }
         }
-
         checkElement();
     }
+
+    function observeUrlChange(urlRegex, callback){
+        let oldHref = document.location.href;
+        const body = document.querySelector("body");
+        const observer = new MutationObserver(mutations => {
+            if (oldHref !== document.location.href) {
+                oldHref = document.location.href;
+                if(urlRegex.test(document.location.href)) {
+                    callback();
+                }
+            }
+        });
+        observer.observe(body, { childList: true, subtree: true });
+        if(urlRegex.test(document.location.href)){
+            callback();
+        }
+    };
+
 
 })();
