b5b10d8644fddd8d2a7daa4dee7ab6149e5c74e2
Bryson Marazzi <42992206+brysonmarazzi@users.noreply.github.com>
2023-10-06 17:48:23 -0700

    UPDATE TO VERSION 7 print_day_sheets.user.js

--- print_day_sheets.user.js
+++ print_day_sheets.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Print All Progress Notes
 // @namespace    http://tampermonkey.net/
-// @version      6.0
+// @version      7.0
 // @description  Print Progress Notes for all Patients
 // @author       Bryson Marazzi
 // @match        https://app.aryaehr.com/aryaehr/clinics/*
